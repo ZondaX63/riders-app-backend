7b8e818ee6fb1ef053642a85014f05f884c5d9a9
ZondaX63 <izzet2003aydin@gmail.com>
2026-01-08 10:28:49 +0300

    commit: fix

--- src/models/User.js
+++ src/models/User.js
@@ -46,6 +46,10 @@ const userSchema = new mongoose.Schema({
     year: {
       type: Number,
       default: 0
+    },
+    imageUrl: {
+      type: String,
+      default: ''
     }
   },
   status: {
@@ -82,9 +86,9 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
-  
+
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
@@ -95,7 +99,7 @@ userSchema.pre('save', async function(next) {
 });
 
 // Method to compare password
-userSchema.methods.comparePassword = async function(candidatePassword) {
+userSchema.methods.comparePassword = async function (candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
 };
 
